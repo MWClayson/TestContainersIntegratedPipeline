a5633bc8060a42512bd564bcf2dedf5887854ca4
Michael Clayson <Michael@Clayson.tech>
2024-08-15 18:52:57 +0100

    Add project files.

---
+++ TestContainersTTLIntegration/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestContainersTTLIntegration
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
